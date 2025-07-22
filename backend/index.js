4a1326141bd6121f23cf8a73c39f6dba49fa0ca2
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-07-21 19:12:01 -0600

    Mongo DB implementation for delete and put methods and error handling

--- backend/index.js
+++ backend/index.js
@@ -10,34 +10,35 @@ app.use(express.static('dist'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
   Person.find({}).then(result => {
     res.json(result)
   })
+  .catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
   const date = new Date
-  const length = persons.length
-
-  res.send(`Phonebook has info for ${length} people <br> ${date}`)
+  Person.find({})
+    .then(result => res.send(`Phonebook has info for ${result.length} people\n${date}`))
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id).then(person => {
     res.json(person)
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  const deletedPerson = persons.find(person => person.id === id)
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(200).json(deletedPerson)
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
   if (!body.name || !body.number) {
@@ -52,7 +53,43 @@ app.post('/api/persons', (req, res) => {
   person.save().then(savedNote => {
     res.json(savedNote)
   })
+  .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body
+
+  if (!name || !number) {
+    return res.status(400).end()
+  }
+
+  Person.findById(req.params.id)
+    .then(person => {
+      if (!person) {
+        return res.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then(updatedPerson => {
+        res.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).json({error: 'malformatted id'})
+  }
+
+  res.status(500).json({error: "Internal Server Error"})
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
